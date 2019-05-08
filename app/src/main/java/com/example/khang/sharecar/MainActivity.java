4af4e144e1e27f9c5ee99449cc9f832fc2492188
khang3016 <hoangkhang0997@gmail.com>
2019-05-08 11:33:19 +0700

    add_layout+bottomnav

--- app/src/main/java/com/example/khang/sharecar/MainActivity.java
+++ app/src/main/java/com/example/khang/sharecar/MainActivity.java
@@ -1,74 +1,71 @@
 package com.example.khang.sharecar;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private ImageButton mSharecar, mRentcar;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-
-                            Intent intent=new Intent(MainActivity.this, DriverMapActivity.class);
-                            startActivity(intent);
-                            finish();
-                       
-                    return true;
-                case R.id.navigation_dashboard:
-                    mSharecar.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent=new Intent(MainActivity.this, DriverMapActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
-                    return true;
-                case R.id.navigation_notifications:
-                    mSharecar.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent=new Intent(MainActivity.this, DriverMapActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
-                    return true;
-            }
-            return false;
-        }
-
-    };
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRentcar=findViewById(R.id.main_act_rentcar);
-        mSharecar=findViewById(R.id.main_act_sharecar);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        //loading the default fragment
+        loadFragment(new DashboardFragment());
 
-
-
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new InfoFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new DashboardFragment();
+                break;
+
+
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
+
+
+
+
+
+
+
+
+
 
 
 
